9716ec60081600eb418f0b6ccf29ef335b2ed275
haruyana <haruka.k1102@gmail.com>
2017-10-13 19:33:23 +0900

    jsonファイルを読込（途中）

--- scripts/main-03.js
+++ scripts/main-03.js
@@ -3,21 +3,22 @@
  */
 
 (function($){
-
   // GoogleMapAPI
+    var map;
     var latlng = new google.maps.LatLng(35.624650, 139.429159);
-    var myOptions = {
+
+    map = new google.maps.Map(document.getElementById('map'),{
       zoom: 6, // 拡大比率
       center: latlng, // マップの中心位置
       mapTypeId: google.maps.MapTypeId.ROADMAP // 表示タイプの指定（衛生写真等に変えられる）
-    };
-    var map = new google.maps.Map(document.getElementById('map'), myOptions);
-
-  // アイコンを設定
-    var map;
+    });
+  // jsonを読込
     var marker = [];
+    var data = [];
     var infoWindow = [];
-    $.getJSON("/scripts/data.json", function(json){
+
+    $.getJSON("scripts/data.json", function(json){
+      alert("a");
       for (var i = 0; i <= json.length-1; i++) {
         data.push(
             {
@@ -27,15 +28,15 @@
             }
           );
         };
-        for (var i = 0; i < markerData.length; i++) {
-          markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
+        for (var i = 0; i < data.length; i++) {
+          markerLatLng = new google.maps.LatLng({lat: json[i]['lat'], lng: json[i]['lng']}); // 緯度経度のデータ作成
           marker[i] = new google.maps.Marker({ // マーカーの追加
           position: markerLatLng, // マーカーを立てる位置を指定
           map: map // マーカーを立てる地図を指定
         });
 
         infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
-           content: '<div class="map">' + markerData[i]['name'] + '</div>' // 吹き出しに表示する内容
+           content: '<div class="map">' + json[i]['name'] + '</div>' // 吹き出しに表示する内容
         });
         markerEvent(i); // マーカーにクリックイベントを追加
        }
