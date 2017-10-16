38b9ccf9ee601530afbd5a891b1df78b49609cb4
haruyana <haruka.k1102@gmail.com>
2017-10-16 14:30:05 +0900

    jsonファイルで外部化

--- scripts/main-03.js
+++ scripts/main-03.js
@@ -5,7 +5,7 @@
 (function($){
   // GoogleMapAPI
     var map;
-    var latlng = new google.maps.LatLng(35.624650, 139.429159);
+    var latlng = new google.maps.LatLng(35.917852, 137.982018);
 
     map = new google.maps.Map(document.getElementById('map'),{
       zoom: 6, // 拡大比率
@@ -15,16 +15,17 @@
   // jsonを読込
     var marker = [];
     var data = [];
+    var icon = [];
     var infoWindow = [];
 
     $.getJSON("scripts/data.json", function(json){
-      alert("a");
       for (var i = 0; i <= json.length-1; i++) {
         data.push(
             {
               'name': json[i].name,
               'lat': json[i].lat,
-              'lng': json[i].lng
+              'lng': json[i].lng,
+              'icon': json[i].icon
             }
           );
         };
@@ -32,21 +33,22 @@
           markerLatLng = new google.maps.LatLng({lat: json[i]['lat'], lng: json[i]['lng']}); // 緯度経度のデータ作成
           marker[i] = new google.maps.Marker({ // マーカーの追加
           position: markerLatLng, // マーカーを立てる位置を指定
-          map: map // マーカーを立てる地図を指定
+          map: map, // マーカーを立てる地図を指定
+          icon: {
+            url: json[i]['icon']// マーカーの画像を変更
+         }
         });
 
         infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
-           content: '<div class="map">' + json[i]['name'] + '</div>' // 吹き出しに表示する内容
+           content: '<div class="ballon_content">' + json[i]['content'] + '</div>' // 吹き出しに表示する内容
         });
         markerEvent(i); // マーカーにクリックイベントを追加
        }
     });
-
-
-     // マーカーにクリックイベントを追加
      function markerEvent(i) {
          marker[i].addListener('click', function() { // マーカーをクリックしたとき
            infoWindow[i].open(map, marker[i]); // 吹き出しの表示
        });
      }
+
 })(jQuery);
